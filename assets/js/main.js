1b6e6aae4ecbd64cb1261839e9b4c99e359e0ac4
Vladimir <fajerus@gmail.com>
2016-11-14 16:25:37 +0300

    Selection participans part

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by fajerus on 14.11.2016.
+ */
+
+function showMenu(){
+  document.getElementById('overlaymenu').style.width = '100%';
+}
+
+function closeMenu() {
+  document.getElementById('overlaymenu').style.width = '0%';
+}
