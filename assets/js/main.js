1701100137af5c2e2de2c669ddc15e2cb8cfcbd6
Vladimir <fajerus@gmail.com>
2016-11-16 10:03:46 +0300

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,35 @@ $(function () {
     $('#overlaymenu').css('width', '0%');
   });
 
+  var photoSwipe;
+
   var mainSwiper = new Swiper('.swiper-container-g', {
-    pagination: '.swiper-pagination-g',
-    paginationClickable: true,
     simulateTouch: false,
     direction: 'vertical',
     nextButton: '.swiper-button-next-g',
     prevButton: '.swiper-button-prev-g',
+    mousewheelControl: true,
     onSlideChangeStart: function (swiper) {
       if(swiper.activeIndex == 0){
         $('.top-navigation').addClass('top-navigation--main');
+        $('.swiper-button-next-g').addClass('swiper-button-next-g--main');
+        mainSwiper.unlockSwipeToNext();
       }else{
         $('.top-navigation').removeClass('top-navigation--main');
+
+        if(swiper.activeIndex != swiper.slides.length-1)
+          $('.swiper-button-next-g').removeClass('swiper-button-next-g--main');
       }
+
+      if(swiper.activeIndex == 1){
+        $('.swiper-button-next-g').addClass('swiper-button-disabled');
+        if(photoSwipe.activeIndex != 2)
+          swiper.lockSwipeToNext();
+
+      }
+
+      if(photoSwipe.activeIndex == 2)
+        $('.swiper-button-next-g').removeClass('swiper-button-disabled');
 
     }
   });
@@ -35,6 +51,12 @@ $(function () {
     simulateTouch: false,
     paginationBulletRender: function (index, className) {
       return '<span class="' + className + ' ' + className + '-numeric">' + (index + 1) + '</span>';
+    },
+    onSlideChangeStart: function (swiper) {
+      if (swiper.activeIndex == 2){
+        $('.swiper-button-next-g').removeClass('swiper-button-disabled');
+        mainSwiper.unlockSwipeToNext();
+      }
     }
   });
 
@@ -51,4 +73,15 @@ $(function () {
     mainSwiper.slideTo(0);
     $('.overlay-menu__closebtn').trigger('click');
   });
+
+  $('#aboutus').click(function () {
+    mainSwiper.slideTo(1);
+    $('.overlay-menu__closebtn').trigger('click');
+  });
+
+  $('#project').click(function () {
+    mainSwiper.slideTo(2);
+    $('.overlay-menu__closebtn').trigger('click');
+  });
+
 });
